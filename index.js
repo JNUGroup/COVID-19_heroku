4526b41cecf946dbc6a262a91e5ce0ee9b97b23b
guohaoyu110 <guohaoyu110@gmail.com>
2020-05-02 18:33:59 -0700

    make it better

--- index.js
+++ index.js
@@ -181,9 +181,13 @@ app.route('/notify').get(function(req,res)
 //   // middleware
 // app.use(forceSSL());
 
-app.use(express.static(__dirname + '/src'));
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+// app.use(express.static(__dirname + '/src'));
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname , 'src','index.html'));
+});
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname , 'src', 'index.html'));
 });
 
 app.listen(port, err => {
