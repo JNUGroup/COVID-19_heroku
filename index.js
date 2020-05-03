667ebc53d0d71857b99f4a847ac37b35f454b46d
guohaoyu110 <guohaoyu110@gmail.com>
2020-05-03 02:41:35 -0700

    make it better

--- index.js
+++ index.js
@@ -170,10 +170,10 @@ app.route('/notify').get(function(req,res)
 
 
 // console.log('__dirname: ', __dirname);
-app.use(express.static(__dirname ));
+app.use(express.static(__dirname + '/dist/covid19-tracker'));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + 'src/index.html'));
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/covid19-tracker/index.html'));
 });
 
 app.listen(process.env.PORT || 7777, () => {
